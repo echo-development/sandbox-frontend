bdfceaa14dcb8beaff743689815eb281383b1a43
Martin Lund <privatmartinlund@gmail.com>
2019-09-15 13:22:56 +0200

    Added basic REST call

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestServiceService } from './test-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  title: String;
+
+  ngOnInit(): void {
+    this.title = "TEH";
+    this.loadTitle();
+  }
+
+  constructor(public testService: TestServiceService){
+    console.log("start");
+  }
+
+  loadTitle() {
+    console.log("Load title");
+    return this.testService.TestGet().subscribe((data :String) => {
+      this.title = data;
+    })
+  }
 }
